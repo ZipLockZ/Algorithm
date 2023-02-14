8573b2eb81161e118e789cdd961d92a26556214d
ivan <gervadin.nomer1@gmail.com>
2023-02-14 21:03:26 +0300

    Добавьте файлы проекта.

---
+++ Login(Algorithm)/Program.cs
@@ -0,0 +1,42 @@
+﻿class Register
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public void Greetings()
+        {
+            Console.WriteLine("Приветствуем вас на нашем сайте," + Name);
+        }
+
+        public void Cheak()
+        {
+            if (IsPremium = false)
+            {
+                ShowAds();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
+    }
+
+}
